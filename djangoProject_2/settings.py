4a2c8942c09cabd7c136dc8d016e1917644a2cf0
akshaybinexture <akshayb.inexture@gmail.com>
2021-05-10 18:35:01 +0530

    changes in setting

--- djangoProject_2/settings.py
+++ djangoProject_2/settings.py
@@ -26,7 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-*wjy=(3km+7)1nt$$7vqm*71y!5b^7(zfw&2@h4o2z#jel!k7f'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+
 
 ALLOWED_HOSTS = ['first-blogapp-django.herokuapp.com']
 
