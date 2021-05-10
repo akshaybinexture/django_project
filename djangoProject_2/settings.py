df3adb5328ce68a5768ec3b16ccda0043f57abe6
akshaybinexture <akshayb.inexture@gmail.com>
2021-05-10 19:04:33 +0530

    changes in setting

--- djangoProject_2/settings.py
+++ djangoProject_2/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os.path
 from pathlib import Path
 from dotenv import load_dotenv
+import django_heroku
 
 
 load_dotenv()
@@ -182,10 +183,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'bajethaakshay@gmail.com'
+EMAIL_HOST_PASSWORD = 'Akshayaap@111'
 # SOCIAL_ACCOUNT_AUTO_SIGNUP = False
-
+django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
