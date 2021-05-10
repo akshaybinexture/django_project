3bac23cd9aca974806ca05d90d3f3b9c3c0d5c3d
akshaybinexture <akshayb.inexture@gmail.com>
2021-05-10 16:54:40 +0530

    fix:changes in Procfile

--- djangoProject_2/settings.py
+++ djangoProject_2/settings.py
@@ -24,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
-
+SECRET_KEY = 'django-insecure-*wjy=(3km+7)1nt$$7vqm*71y!5b^7(zfw&2@h4o2z#jel!k7f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
